94ef8f70c8a12a6675149258369bead665a995f2
harnoorpreetsingh <helloharnoor@gmail.com>
2024-08-29 23:08:22 +0530

    pushing code

---
+++ BackEnd/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bookRoute from "./route/book.route.js"
+import cors from "cors"
+import userRoute from "./route/user.route.js" 
+
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
+
+
+// connect to mongo
+try {
+  mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("connected to mongo");
+} 
+
+catch (error) {
+  console.log("error is", error);
+}
+
+//defining routes
+
+app.use("/book",bookRoute)
+app.use("/user",userRoute)
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
